c15162434bbe97ff7e4778caf80c3691d7f6e1e8
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2023-04-09 21:42:14 +0530

    Add files via upload
    Simple PowerShell script to showcase usage of class, constructor, instance variable and a method. 
    I use Windows PowerShell ISE as my favorite editor to write this PowerShell script. You will need to open PowerShell ISE in Administrator mode to run these scripts within this editor
    If you are running from PowerShell window, run as "Administrator"
    Please elevate your permissions to run above PS script using below command:
    Use Set-ExecutionPolicy unrestricted

---
+++ StartupMain.ps1
@@ -0,0 +1,26 @@
+﻿Set-ExecutionPolicy Unrestricted
+
+## PowerShell class that has a property, constructor and a function to display message to console :)
+class StartupMain
+{
+    # Property that holds single variable of type string
+    [string] $message;
+
+    ## Default Constructor
+    StartupMain([string]$paramMessage)
+    {
+        $this.message = $paramMessage;
+    }
+
+    # Function that displays message. No return values
+    PrintMessage()
+    {
+        Write-Host ("Hello " + $this.message);
+    }
+}
+
+## Technique to instantiate a class
+[StartupMain]$varStartupMain = [StartupMain]::new("PowerShell");
+
+## Technique to invoke a method on an intantiated class
+$varStartupMain.PrintMessage();
