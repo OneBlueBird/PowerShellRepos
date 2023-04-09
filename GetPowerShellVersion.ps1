8c2b22b2a6fc2c1b1c25ff83eaf4ff9026d9ea35
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2023-04-09 21:11:42 +0530

    Add files via upload
    Script to check current PowerShell version in the target environment

---
+++ GetPowerShellVersion.ps1
@@ -0,0 +1,19 @@
+﻿clear;
+
+## How do you gather current version of PowerShell in the target environment ?
+## I use below command to gain insights
+
+Write-Host ("-" *190) -ForegroundColor Cyan;
+$PSVersiontable
+
+Write-Host ("-" *190) -ForegroundColor Cyan;
+Write-Host("Current PowerShell installed version : " + $PSVersiontable.PSVersion) -ForegroundColor Yellow;
+Write-Host ("-" *190) -ForegroundColor Cyan;
+
+
+Write-Host ("PSVersion : " + $PSVersiontable.PSVersion);
+Write-Host ("-" *190) -ForegroundColor Cyan;
+
+Write-Host ("PSVersion Major number : " + $PSVersiontable.PSVersion.Major);
+Write-Host ("PSVersion Minor number : " + $PSVersiontable.PSVersion.Minor);
+Write-Host ("-" *190) -ForegroundColor Cyan;
