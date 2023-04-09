b1aac48f1ee73d8bd86b70606b640efad12a01a4
Saravanakumar.M <saravanakumar.murugesan@gmail.com>
2023-04-09 22:52:05 +0530

    Add files via upload
    To run this PowerShell script, run below command:
    .\UsingPSParameters.ps1 Hello World PowerShell

---
+++ UsingPSParameters.ps1
@@ -0,0 +1,20 @@
+﻿$param1 = $args[0];
+$param2 = $args[1];
+$param3 = $args[2];
+
+function PrintStars()
+{
+    Write-Host("*"* 190) -ForegroundColor Yellow;
+}
+
+
+PrintStars;
+
+Write-Host("Parameter1 passed : " + $param1);
+Write-Host("Parameter2 passwe : " + $param2);
+Write-Host("Parameter2 passwe : " + $param3);
+
+PrintStars;
+
+## How to run this script
+.\UsingPSParameters.ps1 Hello World PowerShell
